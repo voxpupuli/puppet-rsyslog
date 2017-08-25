6b144dcdf179e049dc6992849ed684b60c2f591f
David Hollinger <david.hollinger@moduletux.com>
2017-08-24 22:30:58 -0500

    Add priority default

--- manifests/config/lookup_tables.pp
+++ manifests/config/lookup_tables.pp
@@ -2,8 +2,9 @@ class rsyslog::config::lookup_tables {
   $::rsyslog::config::lookup_tables.each |$table, $config| {
     rsyslog::component::lookup_table { $table:
       * => {
-        'target'  => $::rsyslog::target_file,
-        'confdir' => $::rsyslog::confdir,
+        'priority' => $::rsyslog::lookup_table_priority,
+        'target'   => $::rsyslog::target_file,
+        'confdir'  => $::rsyslog::confdir,
       } + $config,
     }
   }

970437d48f239e3c8a57d24b20d36e48cc210d73
David Hollinger <david.hollinger@moduletux.com>
2019-11-14 17:17:40 -0600

    Added a few tests to check for dependency cycles in action

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -6,4 +6,30 @@ describe 'Rsyslog::Config', include_rsyslog: true do
   classes.each do |clas|
     it { is_expected.to contain_class("rsyslog::config::#{clas}") }
   end
+
+  context 'actions' do
+    let(:pre_condition) { 'include rsyslog' }
+    let(:params) do
+      {
+        actions: {
+          'myaction' => {
+            type: 'omelasticsearch',
+            config: {
+              'queue.type' => 'linkedlist',
+              'queue.spoolDirectory' => '/var/log/rsyslog/queue'
+            }
+          },
+          'myaction2' => {
+            type: 'omfile',
+            facility: 'kern.*',
+            config: {
+              'dynaFile' => 'remoteKern'
+            }
+          }
+        }
+      }
+    end
+
+    it { is_expected.to compile }
+  end
 end

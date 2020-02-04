b9939324bef83f71566dfab381311ad90b2667aa
Jason Galens <bogey.jlg@gmail.com>
2020-02-04 13:56:57 -0500

    Added os facts to action, global and filter types

--- spec/defines/generate_concat_spec.rb
+++ spec/defines/generate_concat_spec.rb
@@ -1,17 +1,22 @@
 require 'spec_helper'
 
 describe 'Rsyslog::Generate_concat', include_rsyslog: true do
-  let(:pre_condition) { 'include rsyslog' }
-  let(:title) { 'mygeneratedconcat' }
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
+      let(:pre_condition) { 'include rsyslog' }
+      let(:title) { 'mygeneratedconcat' }
 
-  let(:params) do
-    {
-      confdir: '/etc/rsyslog.d',
-      target: '50-rsyslog.conf'
-    }
-  end
+      let(:params) do
+        {
+          confdir: '/etc/rsyslog.d',
+          target: '50-rsyslog.conf'
+        }
+      end
 
-  context 'with defaults' do
-    it { is_expected.to contain_concat('/etc/rsyslog.d/50-rsyslog.conf').that_notifies('Service[rsyslog]') }
+      context 'with defaults' do
+        it { is_expected.to contain_concat('/etc/rsyslog.d/50-rsyslog.conf').that_notifies('Service[rsyslog]') }
+      end
+    end
   end
 end

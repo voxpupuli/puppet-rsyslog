ad379d7ee6a419b9de2ea62fad3e0a69814c9f5f
ptiggerdine <ptiggerdine [at] gmail.com>
2021-04-13 10:21:50 +1000

    Remove duplication owner statments.

--- manifests/generate_concat.pp
+++ manifests/generate_concat.pp
@@ -5,22 +5,20 @@ define rsyslog::generate_concat (
   if $rsyslog::manage_service or $rsyslog::external_service {
     if ! defined(Concat["${confdir}/${target}"]) {
       concat { "${confdir}/${target}":
-        owner  => 'root',
+        owner  => $rsyslog::conf_owner_name
+        group  => $rsyslog::conf_group_name,
         notify => Service[$rsyslog::service_name],
         order  => 'numeric',
         mode   => $rsyslog::conf_permissions,
-        owner  => $rsyslog::conf_owner_name,
-        group  => $rsyslog::conf_group_name,
       }
     }
   } else {
     if ! defined(Concat["${confdir}/${target}"]) {
       concat { "${confdir}/${target}":
-        owner => 'root',
-        order => 'numeric',
-        mode  => $rsyslog::conf_permissions,
         owner => $rsyslog::conf_owner_name,
         group => $rsyslog::conf_group_name,
+        order => 'numeric',
+        mode  => $rsyslog::conf_permissions,
       }
     }
   }

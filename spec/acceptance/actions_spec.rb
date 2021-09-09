545c8a5b65766fa4a8f8e98d29a958f58b47467c
Martin Magr <mmagr@redhat.com>
2021-09-09 11:44:43 +0200

    Fix linter issue
    Style/BlockDelimiters: Avoid using {...} for multi-line blocks

--- spec/acceptance/actions_spec.rb
+++ spec/acceptance/actions_spec.rb
@@ -39,10 +39,10 @@ describe 'Rsyslog actions' do
     end
 
     describe file('/etc/rsyslog.d/50_rsyslog.conf') do
-      its(:content) {
+      its(:content) do
         is_expected.to match(%r{# omfile_all_logs\naction\(type="omfile"\n.*name="omfile_all_logs"\n.*queue.type="LinkedList"\n.*queue.spoolDirectory="\/var\/log\/rsyslog\/queue"\n.*file="\/tmp\/log"\n.*\)})
         is_expected.to match(%r{# myaction\naction\(type="omelasticsearch"\n.*name="myaction"\n.*host=\["127.0.0.1", "192.168.0.1"\]\n.*queue.type="linkedlist"\n.*queue.spoolDirectory="\/var\/log\/rsyslog\/queue"\n.*\)})
-      }
+      end
     end
   end
 end

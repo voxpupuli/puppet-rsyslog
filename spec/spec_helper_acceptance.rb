493103de183c41a7662d5d8a9eb70daace9f124d
Christos Papageorgiou <christos.papageorgioy@gmail.com>
2022-05-17 11:58:47 +0300

    fixup! modulesync 5.2.0

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -4,7 +4,6 @@ require 'voxpupuli/acceptance/spec_helper_acceptance'
 
 configure_beaker do |host|
   host.install_package('software-properties-common') if fact_on(host, 'os.name') == 'Ubuntu'
-  host.install_package('lsb-release') if fact_on(host, 'os.name') == 'Ubuntu'
 
   on host, puppet('module', 'uninstall', 'puppetlabs-yumrepo_core', '--force'), acceptable_exit_codes: [0, 1] if ENV['BEAKER_PUPPET_COLLECTION'] != 'puppet6'
 end

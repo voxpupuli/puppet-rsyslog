6c07dd7f46f7c8d0a7ca44f0e215e2cd835d23b8
Yury Bushmelev <jay4mail@gmail.com>
2026-04-27 17:56:44 +0800

    specify missing dependency

--- manifests/base.pp
+++ manifests/base.pp
@@ -1,11 +1,21 @@
 # @summary This class manages the base installation for rsyslog
 class rsyslog::base {
+  $package_res = if $rsyslog::manage_package {
+    package { $rsyslog::package_name:
+      ensure => $rsyslog::package_version,
+    }
+  } else {
+    undef
+  }
+
   if $rsyslog::use_upstream_repo {
     case $facts['os']['family'] {
       'Debian': {
         if $facts['os']['name'] == 'Ubuntu' {
           include apt
-          apt::ppa { 'ppa:adiscon/v8-stable': }
+          apt::ppa { 'ppa:adiscon/v8-stable':
+            before => $package_res
+          }
         }
       }
       'RedHat': {
@@ -16,15 +26,10 @@ class rsyslog::base {
           enabled  => '1',
           gpgcheck => '0',
           gpgkey   => 'http://rpms.adiscon.com/v8-stable/epel-$releasever/$basearch',
+          before   => $package_res,
         }
       }
       default: { fail("${facts['os']['name']} is not currently supported by upstream packages.") }
-    }
-  }
-
-  if $rsyslog::manage_package {
-    package { $rsyslog::package_name:
-      ensure => $rsyslog::package_version,
     }
   }
 

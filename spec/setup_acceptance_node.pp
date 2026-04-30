c72145b72b170288274b5c22835b1bfc1188c3e4
Yury Bushmelev <jay4mail@gmail.com>
2026-04-30 18:49:45 +0800

    add quotes

--- spec/setup_acceptance_node.pp
+++ spec/setup_acceptance_node.pp
@@ -1,9 +1,9 @@
 # Ensure rsyslog is not installed before the suite runs
 service { 'rsyslog': ensure => 'stopped', enable => false }
-package { 'rsyslog-logrotate': ensure => absent }
-package { 'rsyslog': ensure => absent }
-file { '/etc/rsyslog.d': ensure => absent, purge => true, force => true }
-file { '/etc/rsyslog.conf': ensure => absent }
+package { 'rsyslog-logrotate': ensure => 'absent' }
+package { 'rsyslog': ensure => 'absent' }
+file { '/etc/rsyslog.d': ensure => 'absent', purge => true, force => true }
+file { '/etc/rsyslog.conf': ensure => 'absent' }
 
 case $facts['os']['name'] {
   'Ubuntu': {
--- spec/support/acceptance/cleanup_helper.rb
+++ spec/support/acceptance/cleanup_helper.rb
@@ -2,10 +2,10 @@
 
 def wipe_rsyslog
   pp = <<-CLEANUP_MANIFEST
-    package { 'rsyslog-logrotate': ensure => absent }
-    package { 'rsyslog': ensure => absent }
-    file { '/etc/rsyslog.d': ensure => absent, purge => true, force => true }
-    file { '/etc/rsyslog.conf': ensure => absent }
+    package { 'rsyslog-logrotate': ensure => 'absent' }
+    package { 'rsyslog': ensure => 'absent' }
+    file { '/etc/rsyslog.d': ensure => 'absent', purge => true, force => true }
+    file { '/etc/rsyslog.conf': ensure => 'absent' }
   CLEANUP_MANIFEST
 
   apply_manifest(pp, catch_failures: true)
